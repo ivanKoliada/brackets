4fb77ce69f472cf865ed707d716ad155feb071e5
justgrace <ivan.koliada69@gmail.com>
2021-02-28 21:47:47 +0300

    add: task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+   let config = bracketsConfig.map(e => e.join(""));
+   while (config.some(e => str.includes(e))) {
+      for (let i = 0; i < config.length; i++) {
+         str = str.replace(config[i], '');
+      }
+   }
+
+   return Boolean(!str.length);
+};
